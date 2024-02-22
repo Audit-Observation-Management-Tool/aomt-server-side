d9be5bfff8b7a777a0c1a876cea147d3e9ced59b
fariaislam04 <mytul1242@gmail.com>
2024-02-22 20:22:08 +0600

    feat: showing software cards on supervisor dashboard

--- config/database.js
+++ config/database.js
@@ -11,6 +11,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  connectionLimit: 10,
 });
 
 async function checkDatabaseConnection() 
--- index.js
+++ index.js
@@ -3,16 +3,24 @@ const app = express();
 const cors = require('cors');
 app.use(cors(), express.json());
 require('dotenv/config');
+const pool = require("./config/database");
 const SignIn = require("./routes/signinRoute/SignIn");
+const SupervisorDashboard = require("./routes/signinRoute/SupervisorDashboard");
+
 
 async function connectAndStartServer() 
 {
   app.use("/sign-in", SignIn);
-
+  app.use("/supervisor-dashboard", SupervisorDashboard);
   app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server running on port`);
   });
 }
+
+
+
+
+
 
 connectAndStartServer().catch(err => {
   console.error(err);
